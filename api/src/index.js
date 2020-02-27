be3a8778e7cfae28af2c51560cc94998c35eebdc
Florian Kraft <mail@floriankraft.com>
2020-02-27 14:05:20 +0100

    Add some more backend code.

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,29 @@
 const Koa = require('koa');
-const Router = require('@koa/router');
-
+const koaRouter = require('@koa/router');
+const koaBody = require('koa-body');
 const app = new Koa();
-const router = new Router();
 
-router.get('/phonecall', (ctx, next) => {
-  ctx.body = 'phonecall route';
-});
+app.use(koaBody({
+  formidable:{
+    uploadDir: './uploads',
+    keepExtensions: true
+  },
+  multipart: true,
+  urlencoded: true
+}));
 
-router.get('/crawler', (ctx, next) => {
-  ctx.body = 'crawler route';
-});
+const router = new koaRouter();
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+router.post('/phonecall', handlePhonecall);
+
+function *handlePhonecall() {
+  console.log("Files: ", this.request.body.files);
+  console.log("Fields: ", this.request.body.fields);
+  this.body = "Received your data!";
+}
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 console.log('listening on port 3000');
 app.listen(3000);
